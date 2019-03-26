656fce546123efab335f08495f3e59f342227ed4
Park Sung chul <oneyedev@gmail.com>
2019-03-26 16:05:11 +0900

    Change to building Message

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    현재 공사중입니다...
   </div>
 </template>
 <style lang="stylus">
