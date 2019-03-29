71f9204050e3a9b282186ce21d023a79d22b955e
Park Sung chul <oneyedev@gmail.com>
2019-03-29 13:11:42 +0900

    vue-showdown-highlight 적용

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -1,7 +1,18 @@
 <template>
-  <v-container>
-    <v-card class="pa-3">
-      <vue-showdown :markdown="markdown"> </vue-showdown>
+  <v-container :class="$style.container">
+    <v-card
+      :class="{
+        'pa-3': $vuetify.breakpoint.smAndDown,
+        'pa-5': $vuetify.breakpoint.mdAndUp
+      }"
+    >
+      <VueShowdown
+        class="markdown-body"
+        :markdown="markdown"
+        flavor="github"
+        :extensions="['showdownHighlight']"
+      >
+      </VueShowdown>
     </v-card>
   </v-container>
 </template>
@@ -25,8 +36,43 @@ export default {
     } catch (error) {
       next(false)
     }
+  },
+  async beforeRouteUpdate(to, from, next) {
+    try {
+      const module = await import(`@/assets/articles/${to.params.src}.md`)
+      next(vm => {
+        vm.markdown = module.default
+      })
+    } catch (error) {
+      next(false)
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style module>
+.container {
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+}
+@media (max-width: 600px) {
+  .container {
+    padding: 0;
+  }
+}
+pre code {
+  display: inline-block;
+}
+
+code {
+  box-shadow: none;
+  font-weight: 400;
+  color: #333333;
+}
+
+code:before {
+  content: none;
+}
+</style>
--- src/plugins/showdown.js
+++ src/plugins/showdown.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import VueShowdown from 'vue-showdown'
+import VueShowdown, { showdown } from 'vue-showdown'
+import showdownHighlight from 'showdown-highlight'
+import 'highlight.js/styles/github.css'
+import 'github-markdown-css'
+
+showdown.extension('showdownHighlight', showdownHighlight)
 
 Vue.use(VueShowdown, {
-  flavor: 'github',
-  options: {
-    emoji: false
-  }
+  flavor: 'github'
 })
