2d2ccfccffb994d646ae26b6c4a48dc3c0fdc12b
oneyedev <oneyedev@gmail.com>
2019-05-24 23:38:30 +0900

    change to locale string

--- src/mixins/filters.js
+++ src/mixins/filters.js
@@ -2,8 +2,7 @@ export default {
   filters: {
     toDateFormat: function(time) {
       if (!time) return ''
-      const splited = new Date(time).toISOString().split('T')
-      return `${splited[0]} ${splited[1].slice(0, 5)}`
+      return new Date(time).toLocaleString()
     }
   }
 }
