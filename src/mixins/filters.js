8e4fa2475b1adffea172bfcfef97c20778db66c1
oneyedev <oneyedev@gmail.com>
2019-05-24 23:35:31 +0900

    return undefined case

--- src/mixins/filters.js
+++ src/mixins/filters.js
@@ -1,6 +1,7 @@
 export default {
   filters: {
     toDateFormat: function(time) {
+      if (!time) return ''
       const splited = new Date(time).toISOString().split('T')
       return `${splited[0]} ${splited[1].slice(0, 5)}`
     }
