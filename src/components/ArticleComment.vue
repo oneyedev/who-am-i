30f63988186f5270e744bd442af6538c77221e9a
oneyedev <oneyedev@gmail.com>
2019-05-28 11:57:42 +0900

    convert to vue-disqus

--- src/components/ArticleComment.vue
+++ src/components/ArticleComment.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="pa-3" id="disqus_thread"></div>
+  <div class="pa-3">
+    <vue-disqus
+      shortname="personal-blog-for-oneyedev"
+      :identifier="id"
+      :url="`https://oneyedev.github.io/article?id=${id}`"
+      :title="title"
+    ></vue-disqus>
+  </div>
 </template>
 
 <script>
@@ -7,19 +14,6 @@ export default {
   props: {
     id: String,
     title: String
-  },
-  mounted() {
-    const id = this.id
-    const title = this.title
-    window.disqus_config = function() {
-      this.page.url = `https://oneyedev.github.io/article?id=${id}` // Replace PAGE_URL with your page's canonical URL variable
-      this.page.identifier = id // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-      this.page.title = title
-    }
-    var s = document.createElement('script')
-    s.src = 'https://personal-blog-for-oneyedev.disqus.com/embed.js'
-    s.setAttribute('data-timestamp', +new Date())
-    this.$el.appendChild(s)
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import './plugins/showdown'
 import './plugins/axios'
+import './plugins/disqus'
 
 Vue.config.productionTip = false
 
